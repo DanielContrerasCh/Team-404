ab5f2402e53588e4704a5b05bfaf913bed22d7e6
Maria Jose Gaytan Gil <mariajosegaytangil@MacBook-Air-de-Maria-92.local>
2024-03-07 20:55:16 -0600

    encuestas/resolver bug

--- Zebranalytics/controllers/encuestas.controller.js
+++ Zebranalytics/controllers/encuestas.controller.js
@@ -1,5 +1,5 @@
 exports.get_luuna = (request, response, next) =>{
-    response.render('encuesta_luna', {
+    response.render('encuesta_luuna', {
         username: request.session.username || '',
     });
 }
@@ -8,9 +8,22 @@ exports.post_luuna = (request, response, next) =>{
     request.session.username = request.body.username;
 }
 
-
-exports.get_luuna_colchones = (request, response, next) =>{
-    response.render('encuesta_luna_colchones', {
+exports.get_mappa = (request, response, next) =>{
+    response.render('encuesta_mappa', {
         username: request.session.username || '',
     });
+}
+
+exports.post_mappa = (request, response, next) =>{
+    request.session.username = request.body.username;
+}
+
+exports.get_nooz = (request, response, next) =>{
+    response.render('encuesta_nooz', {
+        username: request.session.username || '',
+    });
+}
+
+exports.post_nooz = (request, response, next) =>{
+    request.session.username = request.body.username;
 }
--- Zebranalytics/routes/encuestas.routes.js
+++ Zebranalytics/routes/encuestas.routes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const encuestasController = require('../controllers/encuestas.controller')
 
 router.get('/luuna', encuestasController.get_luuna);
-router.get('/luuna/colchones', encuestasController.get_luuna_colchones);
 router.post('/luuna', encuestasController.post_luuna);
+
+router.get('/mappa', encuestasController.get_mappa);
+router.post('/mappa', encuestasController.post_mappa);
+
+router.get('/nooz', encuestasController.get_nooz);
+router.post('/nooz', encuestasController.post_nooz);
 
 module.exports = router;
