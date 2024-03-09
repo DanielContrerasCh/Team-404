05cbd5c26f09973081e73cf7bb03f0c5876c1043
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-03-09 01:08:17 -0600

    EJS logic for dynamic

--- Zebranalytics/routes/encuestas.routes.js
+++ Zebranalytics/routes/encuestas.routes.js
@@ -2,8 +2,26 @@ const express = require('express');
 const router = express.Router();
 const encuestasController = require('../controllers/encuestas.controller')
 
-router.get('/luuna', encuestasController.get_luuna);
-router.post('/luuna', encuestasController.post_luuna);
+const dinamico = [{
+    textarea: "true",
+    slider: "true",
+    img: "true",
+    simpleInput: "true",
+    multiple: "true"}]; 
+  
+  
+router.get('/luuna', (request, response, next) => {
+    response.render('encuesta_luuna', {
+        dinamico: dinamico,
+    });
+});
+
+router.post('/luuna', (request, response, next) =>{
+    dinamico.push(request.body);  
+    response.redirect('/encuestas/luuna');
+  });
+// router.get('/luuna', encuestasController.get_luuna);
+// router.post('/luuna', encuestasController.post_luuna);
 
 router.get('/mappa', encuestasController.get_mappa);
 router.post('/mappa', encuestasController.post_mappa);
