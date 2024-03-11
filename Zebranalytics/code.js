bfc28cc9e96782be88fe5113177b622834117c36
Maria Jose Gaytan Gil <mariajosegaytangil@MacBook-Air-de-Maria-92.local>
2024-03-10 18:30:40 -0600

    modificacion

--- Zebranalytics/code.js
+++ Zebranalytics/code.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-const db = require('./util/database');
+//const db = require('./util/database');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
