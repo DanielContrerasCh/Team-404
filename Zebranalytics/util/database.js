53b261637703b11ec500af2c504fc8ad93537f61
Maxime VP <a01710550@tec.mx>
2024-03-30 12:23:42 -0600

    merge con develop

--- Zebranalytics/util/database.js
+++ Zebranalytics/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'zebranalytics',
-    password: '',
+    password: '5569',
     multipleStatements: true,
 });
 
