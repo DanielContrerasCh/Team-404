dc7ed558eab1967a59ab2a03ed8463e4a7075eba
Maria Jose Gaytan Gil <A01706616@tec.mx>
2024-04-01 07:44:53 -0600

    comentarios para dividir mejor el código

--- Zebranalytics/routes/encuestas.routes.js
+++ Zebranalytics/routes/encuestas.routes.js
@@ -4,10 +4,12 @@ const isAuth = require('../util/is-auth');
 const canUpdate = require('../util/can-update-reviews');
 const encuestasController = require('../controllers/encuestas.controller')
 
+// Vistas de marcas
 router.get('/luuna', isAuth, canUpdate, encuestasController.get_luuna);
 router.get('/mappa', isAuth, canUpdate, encuestasController.get_mappa);
 router.get('/nooz', isAuth, canUpdate, encuestasController.get_nooz);
 
+// Rutas para Luuna
 router.get('/luuna_new_colchones', isAuth, canUpdate, encuestasController.get_luuna_new_colchones);
 router.post('/luuna_new_colchones', isAuth, canUpdate, encuestasController.post_luuna_new_colchones);
 
@@ -23,6 +25,7 @@ router.post('/luuna_new_blancos', isAuth, canUpdate, encuestasController.post_lu
 router.get('/luuna_new_ninos', isAuth, canUpdate, encuestasController.get_luuna_new_ninos);
 router.post('/luuna_new_ninos', isAuth, canUpdate, encuestasController.post_luuna_new_ninos);
 
+//Rutas para Mappa
 router.get('/mappa_new_maletas', isAuth, canUpdate, encuestasController.get_mappa_new_maletas);
 router.post('/mappa_new_maletas', isAuth, canUpdate, encuestasController.post_mappa_new_maletas);
 
@@ -31,5 +34,7 @@ router.post('/mappa_new_mochilas', isAuth, canUpdate, encuestasController.post_m
 
 router.get('/mappa_new_accesorios', isAuth, canUpdate, encuestasController.get_mappa_new_accesorios);
 router.post('/mappa_new_accesorios', isAuth, canUpdate, encuestasController.post_mappa_new_accesorios);
+
+//Rutas para Nooz
 
 module.exports = router;
