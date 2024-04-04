5f1824dbce1e1bcff3f3b0638b7516e3a68c55e6
Maria Jose Gaytan Gil <A01706616@tec.mx>
2024-04-04 07:11:48 -0600

    modificacion a la base de datos para manejar las opciones de preguntas (múltiple y checkbox)

--- Zebranalytics/util/database.js
+++ Zebranalytics/util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'zebranalytics',
-    password: '',
+    database: 'proyecto',
+    password: 'M.j.15102004',
     multipleStatements: true,
 });
 
