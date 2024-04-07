e078aee20280f73b4adb3b4d56d7620ea3474492
Maxime VP <a01710550@tec.mx>
2024-04-06 20:12:30 -0600

    Ajuste de permisos en la página de brands

--- Zebranalytics/routes/brands.routes.js
+++ Zebranalytics/routes/brands.routes.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../util/is-auth');
+const canAdmin = require('../util/can-admin');
 const canUpdate = require('../util/can-update-reviews');
+
 const brandsController = require('../controllers/brands.controller')
 
 router.get('/', isAuth, canUpdate, brandsController.get_brands);
 router.post('/', isAuth, canUpdate, brandsController.post_brands);
 
 
-router.get('/new', isAuth, canUpdate, brandsController.get_new_brands);
-router.post('/new', isAuth, canUpdate, brandsController.post_new_brands);
+router.get('/new', isAuth, canAdmin, brandsController.get_new_brands);
+router.post('/new', isAuth, canAdmin, brandsController.post_new_brands);
 
 
-router.get('/delete', isAuth, canUpdate, brandsController.get_delete_brands);
-router.post('/delete', isAuth, canUpdate, brandsController.post_delete_brands);
+router.get('/delete', isAuth, canAdmin, brandsController.get_delete_brands);
+router.post('/delete', isAuth, canAdmin, brandsController.post_delete_brands);
 
 
-router.get('/editname', isAuth, canUpdate, brandsController.get_edit_brands_name);
-router.post('/editname', isAuth, canUpdate, brandsController.post_edit_brands_name);
+router.get('/editname', isAuth, canAdmin, brandsController.get_edit_brands_name);
+router.post('/editname', isAuth, canAdmin, brandsController.post_edit_brands_name);
 
-router.get('/editimage', isAuth, canUpdate, brandsController.get_edit_brands_image);
-router.post('/editimage', isAuth, canUpdate, brandsController.post_edit_brands_image);
+router.get('/editimage', isAuth, canAdmin, brandsController.get_edit_brands_image);
+router.post('/editimage', isAuth, canAdmin, brandsController.post_edit_brands_image);
 
 module.exports = router;
