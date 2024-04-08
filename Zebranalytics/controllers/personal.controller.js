94ed2425c70238a9040ac6e2d7d70a4b3227802e
Maxime VP <a01710550@tec.mx>
2024-04-07 21:58:26 -0600

    Problemas al eliminar usuarios y modificar rol dinamicamente solucionados

--- Zebranalytics/controllers/personal.controller.js
+++ Zebranalytics/controllers/personal.controller.js
@@ -38,17 +38,21 @@ exports.post_personal = (request, response, next) =>{
 exports.post_delete_personal = (request, response, next) =>{
     Usuario.delete(request.body.correo)
         .then(([rows, fieldData]) => {
-            response.redirect('personal');
+            response.redirect('/personal');
         })
         .catch((error) => {
             console.log(error)
             request.session.error = 'Error al borrar';
-            response.redirect('personal');
+            response.redirect('/personal');
         })
 }
 
 exports.post_modify_personal = (request, response, next) =>{
+    console.log("Entra a post_modify_personal")
+    console.log(request.body.correo)
+    console.log(request.body.rol)
     Usuario.modify(request.body.correo, request.body.rol)
+        
         .then(([rows, fieldData]) => {
             response.redirect('/personal');
         })
