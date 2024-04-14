d29142b626005a11fe3e855f0354665782ee5fa9
Maxime VP <a01710550@tec.mx>
2024-04-13 23:12:10 -0600

    Se solucionó problema que guardaba imagenes cuando no debia

--- Zebranalytics/models/marca.model.js
+++ Zebranalytics/models/marca.model.js
@@ -22,157 +22,41 @@ module.exports = class Marca {
     }
     
     save() {
+
         return db.execute("INSERT INTO imagenmarca VALUES (?, ?)", 
             [this.nombre, this.imagen]
             
             // [this.nombre, this.imagen]
             )
         .catch((error => {
+
+            const rutaImagen = this.imagen;
+            console.log(rutaImagen);
+    
+            // Construir la ruta completa del archivo
+            const rutaCompleta = path.join('public/img/', rutaImagen);
+    
+            console.log(rutaCompleta)
+            // Verificar si el archivo existe antes de intentar eliminarlo
+            if (fs.existsSync(rutaCompleta)) {
+                // Intentar eliminar el archivo
+                try {
+                    // Eliminar el archivo
+                    fs.unlinkSync(rutaCompleta);
+                    console.log('El archivo fue eliminado correctamente.');
+                } catch (error) {
+                    console.error('Error al eliminar el archivo borrar manualmente:', error);
+                }
+            } else {
+                console.log('El archivo de la imagen no existe en el sistema de archivos.');
+            }
+
+
             console.log(error)
             throw Error('Error al guardar');
         }));
     }
 
-    // static delete(marca) {
-    //     return db.execute(`SELECT imagen FROM imagenmarca WHERE nombre = ?`, [marca])
-    //         .then(([rows, fields]) => {
-    //             if (rows.length === 0) {
-    //                 throw new Error('Marca no encontrada');
-    //             }
-    //             const rutaImagen = rows[0].imagen;
-    
-    //             // Construir la ruta completa del archivo
-    //             const rutaCompleta = path.join('public/img/', rutaImagen);
-
-    //             console.log(rutaCompleta)
-    //             // Eliminar el archivo
-    //             fs.unlink(rutaCompleta, (error) => {
-    //                 if (error) {
-    //                     console.error('Error al eliminar el archivo');
-    //                     throw new Error('Error al eliminar el archivo');
-    //                 }
-    //                 console.log('El archivo fue eliminado correctamente.');
-    
-    //                 // Luego de eliminar el archivo, eliminar la fila de la base de datos
-    //                 return db.execute(`DELETE FROM imagenmarca WHERE nombre = ?`, [marca]);
-    //             });
-    //         })
-    //         .catch(error => {
-    //             console.error(error);
-    //             throw new Error('Error al eliminar la marca o el archivo');
-    //         });
-    // }
-
-    // Método para eliminar una categoría por su nombre y marca
-    // static async delete(nombreMarca) {
-    //     const connection = await db.getConnection();
-    //     try {
-    //         await connection.beginTransaction(); // Inicia una transacción
-
-        
-            
-    //         const [nombreCategorias] = await connection.execute(
-    //             `SELECT categoria_nombre FROM categorias WHERE nombre_marca = ?`,
-    //             [nombreMarca]
-                
-    //         );
-            
-            
-    //         for (const categoria of nombreCategorias) {
-
-    //             // Obtener IDs de preguntas asociadas a la categoría
-    //             console.log(categoria)
-    //             console.log(nombreMarca);
-
-    //             const [preguntas] = await connection.execute(
-    //                 `SELECT IDPreguntas FROM preguntas WHERE NombreMarca = ? AND Categoria = ?`
-    //                 [nombreMarca, categoria.categoria_nombre]
-    //             );
-
-    //             console.log("Preguntas: ", preguntas)
-        
-    //             // Eliminar opciones de las preguntas encontradas
-    //             for (const pregunta of preguntas) {
-    //                 await connection.execute(
-    //                     'DELETE FROM opciones_pregunta WHERE IDPreguntas = ?',
-    //                     [pregunta.IDPreguntas]
-    //                 );
-    //             }
-
-    //             // Eliminar las preguntas asociadas a la categoría si las hay
-    //             if (preguntas.length > 0) {
-    //                 await connection.execute(
-    //                     'DELETE FROM preguntas WHERE NombreMarca = ? AND Categoria = ?',
-    //                     [nombreMarca, categoria]
-    //                 );
-    //             }
-
-    //             // Eliminar las categorias asociadas a la marca si las hay (deberia de haber pues se itera sobre las categorias existentes)
-    //             if (nombreCategorias.length > 0){
-    //                 await connection.execute(
-    //                     'DELETE FROM categorias WHERE NombreMarca = ? AND categoria_nombre = ?',
-    //                     [nombreMarca, categoria]
-    //                 );
-    //             }
-
-    //         }
-
-
-    //         // Tratar de eliminar la imagen de los archivos (de no ser posible enviar mensaje de error y continuar con la eliminacion)
-
-    //         // trycatch de eliminar imagen, si no se puede eliminar se pide que se continue con la eliminación de marca
-        
-    //         try {
-    //             const [rows, fields] = await connection.execute(`SELECT imagen FROM imagenmarca WHERE nombre = ?`, [nombreMarca]);
-        
-    //             if (rows.length === 0) {
-    //                 throw new Error('Marca no encontrada');
-    //             }
-        
-    //             const rutaImagen = rows[0].imagen;
-        
-    //             // Construir la ruta completa del archivo
-    //             const rutaCompleta = path.join('public/img/', rutaImagen);
-        
-    //             console.log(rutaCompleta);
-        
-    //             // Eliminar el archivo
-    //             await new Promise((resolve, reject) => {
-    //                 fs.unlink(rutaCompleta, (error) => {
-    //                     if (error) {
-    //                         console.error('Error al eliminar el archivo');
-    //                         reject(new Error('Error al eliminar el archivo'));
-    //                         return;
-    //                     }
-    //                     console.log('El archivo fue eliminado correctamente.');
-    //                     resolve();
-    //                 });
-    //             });
-        
-    //             console.log('El archivo fue eliminado correctamente.');
-
-    //         } catch (error) {
-    //             console.error(error);
-    //             throw new Error('Error al eliminar el archivo');
-    //         }
-
-    //         // Finalmente, eliminar la marca
-    //         await connection.execute(
-    //             'DELETE FROM imagenmarca WHERE nombre_marca = ?',
-    //             [nombreMarca]
-    //         );
-
-    //         console.log("antes del commit");
-
-    //         await connection.commit(); // Confirma los cambios si todo va bien
-    //     } catch (error) {
-    //         console.log(error);
-    //         await connection.rollback(); // Revierte los cambios en caso de error
-    //         throw new Error('Error al eliminar la marca, sus categoría y sus preguntas y opciones asociadas');
-    //     } finally {
-    //         connection.release(); // Libera la conexión
-    //     }
-    // }
 
     static async constructImagePath(nombre) {
         console.log("Nombre de la marca recibido:", nombre);
@@ -223,7 +107,7 @@ module.exports = class Marca {
         } catch (error) {
             await connection.rollback(); // Si hay un error, revierte los cambios
             console.log(error);
-            throw new Error('Error al renombrar la categoría y actualizar la tabla de preguntas');
+            throw new Error('Error al eliminar marca');
         } finally {
             connection.release(); // Libera la conexión
         }
@@ -271,7 +155,7 @@ module.exports = class Marca {
                         console.log('Imagen nueva eliminada');
                         if (error) {
                             alert('Error al eliminar la imagen nueva')
-                            console.error('Error al eliminar la imagen nueva:');
+                            console.error('Error al cambiar imagen, eliminar la imagen nueva manualmente:');
                         }
                     });
                     
@@ -298,7 +182,7 @@ module.exports = class Marca {
             })
             .catch(error => {
                 console.error(error);
-                throw new Error('Error al actualizar la imagen marca');
+                throw new Error('Error al actualizar la imagen de la marca');
             });
     }
 
