0a950397e6c15509e34ca2d5c4af1e3913d55c36
Ethan Luna <ethanlunacado@gmail.com>
2024-04-16 22:43:22 -0600

    HotFix para eliminar un modelo deprecado

--- Zebranalytics/controllers/analiticas.controller.js
+++ Zebranalytics/controllers/analiticas.controller.js
@@ -2,26 +2,14 @@ const { response } = require('express');
 const Analiticas = require('../models/analiticas.model')
 
 exports.getAnaliticas = (request, response, next) => {
-    Analiticas.fetchAnalytics()
-        .then(({ analytics }) => { // Acceder a la propiedad 'analytics'
             response.render('analiticas' , {
-                analytics: analytics, 
                 username: request.session.username || '',
                 csrfToken: request.csrfToken(),
                 permisos: request.session.permisos || [],
             })
             //console.log(analytics);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
 }
-exports.post_analiticas = (request, response, next) =>{
-    response.render('analiticas', {
-        csrfToken: request.csrfToken(),
-        permisos: request.session.permisos || [],
-    });
-}
+
 
 exports.getSomeAnalyticsbyBrandAndYear = (request, response, next) => {
     const brand = request.body.brand; // Obtener la marca de la petición
--- Zebranalytics/routes/analiticas.routes.js
+++ Zebranalytics/routes/analiticas.routes.js
@@ -5,7 +5,6 @@ const canSee = require('../util/can-see');
 const analiticasController = require('../controllers/analiticas.controller')
 
 router.get('/', isAuth, canSee, analiticasController.getAnaliticas);
-router.post('/', isAuth, canSee, analiticasController.post_analiticas);
 router.post('/filteredAnalyticsBybrandAndYear', isAuth, canSee, analiticasController.getSomeAnalyticsbyBrandAndYear);
 router.post('/filteredAnalyticsByitemCode', isAuth, canSee, analiticasController.getSomeAnalyticsbyItemCode);
 module.exports = router;
