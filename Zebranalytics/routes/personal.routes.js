53eb367d7b88b17f4514169d8b8fae8c805eacf1
Daniel Contreras <danicontreras0409@gmail.com>
2024-04-18 13:43:44 -0600

    quitar auth test

--- Zebranalytics/routes/personal.routes.js
+++ Zebranalytics/routes/personal.routes.js
@@ -4,8 +4,8 @@ const isAuth = require('../util/is-auth');
 const canAdmin = require('../util/can-admin');
 const personalController = require('../controllers/personal.controller')
 
-router.get('/', isAuth, canAdmin, personalController.get_personal);
-router.post('/', isAuth, canAdmin, personalController.post_personal);
+router.get('/',  personalController.get_personal);
+router.post('/',  personalController.post_personal);
 
 router.post('/delete', isAuth, canAdmin, personalController.post_delete_personal)
 router.post('/modify', isAuth, canAdmin, personalController.post_modify_personal)
