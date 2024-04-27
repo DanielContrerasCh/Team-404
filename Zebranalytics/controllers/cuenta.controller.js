273d6d39dd7f1282f12455de26c9e9c964b5f8c3
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-04-26 22:44:05 -0600

    Add verificación de cambio de contraseña en account

--- Zebranalytics/controllers/cuenta.controller.js
+++ Zebranalytics/controllers/cuenta.controller.js
@@ -3,11 +3,15 @@ const Usuario = require('../models/usuario.model');
 exports.get_cuenta = (request, response, next) =>{
     const error = request.session.error;
     request.session.error = '';
+    const success = request.session.success;
+    request.session.success = '';
     response.render('account', {
         username: request.session.username || '',
         csrfToken: request.csrfToken(),
         permisos: request.session.permisos || [],
         error: error,
+        success: success,
+
     });
 }
 
@@ -19,5 +23,6 @@ exports.post_cuenta = (request, response, next) =>{
 
 exports.post_cuenta_editar = (request, response, next) =>{
     Usuario.changePassword(request.session.correo, request.body.password);
+    request.session.success = 'Contraseña cambiada con éxito';
     response.redirect('/cuenta')
 }
