28d7bf0a9b9dfe016c8c70c94dad888893a73035
Daniel Contreras <danicontreras0409@gmail.com>
2024-04-30 12:29:13 -0600

    Checar si getTime() regresa algo

--- Zebranalytics/cronjob/cronjob.controller.js
+++ Zebranalytics/cronjob/cronjob.controller.js
@@ -3,7 +3,12 @@ const zecore = require('../controllers/zecore.controller');
 const Cronjob = require('./cronjob.model');
 
 exports.checkTime = async () => {
-    const { name, email, itemCode } =  await Cronjob.getTime();
-    const idResena = await Cronjob.getReview(itemCode, email);
-    await  zecore.postMail(name, email, itemCode, idResena);
+    const cronjobResult =  await Cronjob.getTime();
+    if(cronjobResult) {
+        const { name, email, itemCode } = cronjobResult;
+        const idResena = await Cronjob.getReview(itemCode, email);
+        await  zecore.postMail(name, email, itemCode, idResena);
+    } else {
+        console.log('No results');
+    }
 }
